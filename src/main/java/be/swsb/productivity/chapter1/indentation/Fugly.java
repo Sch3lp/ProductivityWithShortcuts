71ef8104b05c517570afd0e2a6b5f7e81d5b40ae
Sch3lp <sch3lp@gmail.com>
2016-04-20 13:35:29 +0200

    fixed errors

--- src/main/java/be/swsb/productivity/chapter1/indentation/Fugly.java
+++ src/main/java/be/swsb/productivity/chapter1/indentation/Fugly.java
@@ -25,10 +25,4 @@ public class Fugly {
                 .toString());
     }
 
-    public static void wrapStuff()
-        fugly()
-                .withEff(efffff)
-                .withYew "yewwww"
-                .build();
-
 }
